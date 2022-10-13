a5ed70d61a3e09500d75039fc341e06f37b83420
Richardo <Your Email>
2022-10-13 14:57:35 +0700

    Signed-off-by: Richardo <Your Email>

--- index.php
+++ index.php
@@ -12,7 +12,20 @@ include "koneksi.php"
 </head>
 
 <body>
-    
+<h1>Input Database Project</h1>
+    <!-- Insert Section -->
+    <div class="form">
+        <form Method="POST" action="">
+            <div class="nim">NIM Mahasiswa : <input type="number" name="NIM"></div>
+            <div class="name">Nama Mahasiswa : <input type="text" name="nama"></div>
+            <div class="address"> Alamat Mahasiswa: <input type="text" name="alamat"></div>
+            <div class="sumbit"><input type="submit" value="submit" name="submit">
+                <input type="reset" value="reset">
+            </div>
+
+            <br>
+        </form>
+    </div>
     <?php
     if (isset($_POST['submit'])) {
         $nim = $_POST['NIM'];
@@ -29,44 +42,44 @@ include "koneksi.php"
     }
 
     ?>
-    <table>
+    <table border="2">
         <thead>
             <tr>
                 <th>NIM</th>
                 <th>Nama Mahasiswa</th>
                 <th>Alamat Mahasiswa</th>
-                <th colspan = 3>Action</th>
+                <th colspan=3>Action</th>
             </tr>
         </thead>
         <tbody>
-        <?php
-            $sql = "SELECT * FROM tbmahasiswa";
-            $tampil = mysqli_query($conn,$sql);
-            while($data=mysqli_fetch_array($tampil)):
-        ?>
-        <tr>
-        <td><?=$data['NIM'];?></td>
-        <td><?=$data['Nama'];?></td>
-        <td><?=$data['Alamat'];?></td>
-        <td><button><a href="edit.php?hal=edit&id=<?=$data['NIM'];?>">Edit</a></button></td>
-        <td><button><a href="index.php?hal=hapus&id=<?=$data['NIM'];?>">Hapus</a></button></td>
-        </tr>
-        <?php endwhile;?>
-        </tbody>
-        <?php
-            if(isset($_GET['hal']))
-            {
-                if($_GET['hal']=="hapus")
-                {
-                    $hapus=mysqli_query($conn,"DELETE FROM tbmahasiswa WHERE NIM = '$_GET[id]'");
-                    if ($hapus) {
-                        echo  "<script>alert('Hapus Data Berhasil');</script>";
-                    } else {
-                        echo "<script>alert('Hapus Data Gagal');</script>";
-                    }
 
+            <!-- View Section -->
+            <?php
+            $sql = "SELECT * FROM tbmahasiswa";
+            $tampil = mysqli_query($conn, $sql);
+            while ($data = mysqli_fetch_array($tampil)) :
+            ?>
+                <tr>
+                    <td><?= $data['NIM']; ?></td>
+                    <td><?= $data['Nama']; ?></td>
+                    <td><?= $data['Alamat']; ?></td>
+                    <td><button><a href="edit.php?hal=edit&id=<?= $data['NIM']; ?>">Edit</a></button></td>
+                    <td><button><a href="index.php?hal=hapus&id=<?= $data['NIM']; ?>">Hapus</a></button></td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+        <!-- Delete Section -->
+        <?php
+        if (isset($_GET['hal'])) {
+            if ($_GET['hal'] == "hapus") {
+                $hapus = mysqli_query($conn, "DELETE FROM tbmahasiswa WHERE NIM = '$_GET[id]'");
+                if ($hapus) {
+                    echo  "<script>alert('Hapus Data Berhasil');</script>";
+                } else {
+                    echo "<script>alert('Hapus Data Gagal');</script>";
                 }
             }
+        }
         ?>
     </table>
 </body>
